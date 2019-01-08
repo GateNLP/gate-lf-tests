d6b56736dc882fa87e02904bc49d1dd602c59a10
Johann Petrak <johann.petrak@gmail.com>
2019-01-08 17:18:11 +0000

    WIP

---
+++ delete-backend-dirs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Remove the backend software directories in all model directories in order to allow them
+# getting updated to the newest version when training is run.
+
+find . -type d -name FileJsonPyTorch | xargs -n 1 rm -rf
