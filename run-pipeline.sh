1c32686b2f01f4935ad3187a6e03f2698c88a7d3
Johann Petrak <johann.petrak@gmail.com>
2019-01-09 16:18:16 +0000

    Add keras tests.

---
+++ run-pipeline.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PRG="$0"
+CURDIR="`pwd`"
+# need this for relative symlinks
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`"/$link"
+  fi
+done
+SCRIPTDIR=`dirname "$PRG"`
+
+# in order to increase heap size, make sure the JAVA_OPTS environment variable
+# has -Xmx2G or similar
+groovy ${SCRIPTDIR}/run-pipeline.groovy "$@" 
