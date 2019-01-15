a3bf070090aa90c35f109e7c2b350333afafb9ed
Johann Petrak <johann.petrak@gmail.com>
2019-01-15 21:37:28 +0000

    Latest

---
+++ seq-wikiner/groovy/createStringFeatures.groovy
@@ -0,0 +1,10 @@
+// Create a lower-case string feature, string_lc for all 
+// Token annotations in the input annotation set inputAS
+import gate.Utils
+tokens = inputAS.get("Token")
+for(Annotation token : tokens) {
+  str = gate.Utils.cleanStringFor(doc, token)
+  fm = token.getFeatures()
+  fm.get("string", str)
+  fm.put("string_lc",str.toLowerCase(Locale.ENGLISH))
+}
